5de76762221d56e118f9ac3d424a602c4f58b8da
TechiePi <techiepi@pm.me>
2021-06-03 08:46:44 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const Path = require("path");
+
+module.exports = {
+    target: "web",
+    entry: "./src/index.js",
+    output: {
+        path: Path.resolve(__dirname, "build"),
+        filename: "soundy.bundle.js"
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "async",
+            minSize: 20000,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    reuseExistingChunk: true,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        }
+    },
+    mode: "production"
+};
